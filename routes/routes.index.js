0fe7ac57887320d1cc480d5538807262d4474115
j3elam <j3elam@gmail.com>
2019-04-17 22:40:22 -0500

    Add ?

--- routes/routes.index.js
+++ routes/routes.index.js
@@ -1,4 +1,4 @@
 module.exports = function (app) {
   app.use('/api/', require('./routes.unauth'));
-  app.use('/api/new-chat/:email', GitController.newChat(req))
+  app.use('/api/new-chat/:email', GitController.newChat(req)) //does this work? not sure how req will get passed here
 };
