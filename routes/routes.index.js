0dd15a858c417c0dd3c75e8272e44069a8c7f7d3
j3elam <j3elam@gmail.com>
2019-04-17 22:06:25 -0500

    update new chat router

--- routes/routes.index.js
+++ routes/routes.index.js
@@ -1,3 +1,4 @@
 module.exports = function (app) {
-    app.use('/api/', require('./routes.unauth'))
+  app.use('/api/', require('./routes.unauth'));
+  app.use('/api/new-chat/:email', GitController.newChat(req))
 };
